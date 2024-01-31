d7b5424290a120127cecfb4205b7ac15d498dad7
Tatarinov_Artem_3PK1 <artem228suk@gmail.com>
2024-01-31 18:46:39 +0300

    pz14
    pz14

---
+++ pz14/pz14/pz14.cpp
@@ -0,0 +1,81 @@
+﻿// pz14.cpp : Определяет точку входа для приложения.
+//
+
+#include "framework.h"
+#include "pz14.h"
+
+#define STRICT
+#define WIN32_LEAN_AND_MEAN
+
+#include <windows.h>
+
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT umsg, WPARAM wparam, LPARAM lparam);
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+    HWND hwnd;
+    MSG msg;
+    WNDCLASSEX wc;
+
+    // определение оконного класса
+    wc.cbSize = sizeof(WNDCLASSEX);
+    wc.style = CS_HREDRAW | CS_VREDRAW;
+    wc.lpfnWndProc = WindowProc;
+    wc.cbClsExtra = 0;
+    wc.cbWndExtra = 0;
+    wc.hInstance = hInstance;
+    wc.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+    wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+    wc.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+    wc.lpszMenuName = NULL;
+    wc.lpszClassName = L"myclass";
+    wc.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
+
+    if (!RegisterClassEx(&wc))
+    {
+        MessageBox(NULL, L"Регистрация окна не удалась!", L"Ошибка!", MB_ICONEXCLAMATION | MB_OK);
+        return 0;
+    }
+
+    // создание окна
+    hwnd = CreateWindowEx(
+        WS_EX_CLIENTEDGE,
+        L"myclass",
+        L"Моё первое окно!",
+        WS_OVERLAPPEDWINDOW,
+        CW_USEDEFAULT, CW_USEDEFAULT, 800, 500,
+        NULL, NULL, hInstance, NULL);
+
+    if (hwnd == NULL)
+    {
+        MessageBox(NULL, L"Ошибка создания окна!", L"Ошибка!", MB_ICONEXCLAMATION | MB_OK);
+        return 0;
+    }
+
+    ShowWindow(hwnd, nCmdShow);
+    UpdateWindow(hwnd);
+
+    // цикл обработки сообщений
+    while (GetMessage(&msg, NULL, 0, 0))
+    {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+
+    return (int)msg.wParam;
+}
+
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT umsg, WPARAM wparam, LPARAM lparam)
+{
+    switch (umsg)
+    {
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        break;
+
+    default:
+        return DefWindowProc(hwnd, umsg, wparam, lparam);
+    }
+
+    return 0;
+}
